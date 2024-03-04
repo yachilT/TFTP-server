9568f7dc0f80acfdf1be945976d51f81552742f7
Tom Yachil <yachil@post.bgu.ac.il>
2024-03-04 20:39:33 +0200

    implemented encoder decoder

--- server/src/main/java/bgu/spl/net/impl/tftp/TftpEncoderDecoder.java
+++ server/src/main/java/bgu/spl/net/impl/tftp/TftpEncoderDecoder.java
@@ -1,21 +1,37 @@
 package bgu.spl.net.impl.tftp;
 
+import java.util.LinkedList;
+
 import bgu.spl.net.api.MessageEncoderDecoder;
 
 public class TftpEncoderDecoder implements MessageEncoderDecoder<byte[]> {
     //TODO: Implement here the TFTP encoder and decoder
+    final byte endByte = 0;
+    final LinkedList<Byte> currentBytes;
 
+    public TftpEncoderDecoder() {
+        currentBytes = new LinkedList<>();
+    }
 
     @Override
     public byte[] decodeNextByte(byte nextByte) {
-        // TODO: implement this
-
+        currentBytes.add(nextByte);
+        if (nextByte == endByte && !currentBytes.isEmpty()) {
+            
+            byte[] res = new byte[currentBytes.size()];
+            int i = 0;
+            for (Byte b : currentBytes) {
+                res[i] = b.byteValue();
+                i++;
+            }
+            return res;
+        }
         return null;
+
     }
 
     @Override
     public byte[] encode(byte[] message) {
-        //TODO: implement this
-        return null;
+        return message;
     }
 }
