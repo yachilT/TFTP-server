1014f1193ef3625453f05b67f6b1829c155f3695
roioha <roioha@post.bgu.ac.il>
2024-03-06 22:20:19 +0200

    implemented Server and handler

---
+++ server/src/main/java/bgu/spl/net/impl/tftp/TftpBlockingConnectionHandler.java
@@ -0,0 +1,63 @@
+package bgu.spl.net.impl.tftp;
+
+import bgu.spl.net.api.BidiMessagingProtocol;
+import bgu.spl.net.api.MessageEncoderDecoder;
+import bgu.spl.net.srv.ConnectionHandler;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class TftpBlockingConnectionHandler<T> implements Runnable, ConnectionHandler<T> {
+
+    private final BidiMessagingProtocol<T> protocol;
+    private final MessageEncoderDecoder<T> encdec;
+    private final Socket sock;
+    private BufferedInputStream in;
+    private BufferedOutputStream out;
+    private volatile boolean connected = true;
+
+    public TftpBlockingConnectionHandler(Socket sock, MessageEncoderDecoder<T> reader, BidiMessagingProtocol<T> protocol) {
+        this.sock = sock;
+        this.encdec = reader;
+        this.protocol = protocol;
+    }
+
+    @Override
+    public void run() {
+        try (Socket sock = this.sock) { //just for automatic closing
+            int read;
+
+            in = new BufferedInputStream(sock.getInputStream());
+            out = new BufferedOutputStream(sock.getOutputStream());
+
+            while (!protocol.shouldTerminate() && connected && (read = in.read()) >= 0) {
+                T nextMessage = encdec.decodeNextByte((byte) read);
+                if (nextMessage != null) {
+                    protocol.process(nextMessage);
+                }
+            }
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void close() throws IOException {
+        connected = false;
+        sock.close();
+    }
+
+    @Override
+    public void send(T msg) {
+        if (msg != null) 
+            try {
+                out.write(encdec.encode(msg));
+                out.flush();
+            } catch (IOException e) {}
+        
+    }
+}
